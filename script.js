10c3f592a340bdfc5f170afb7a1c394f0e09e3c9
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2024-06-01 11:23:38 +0530

    d

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
             if(gameSpeed > 30) {
                 clearInterval(intervalId);
 
-                gameSpeed -= 1
+                gameSpeed -= 30
 
                 gameLoop();
 
